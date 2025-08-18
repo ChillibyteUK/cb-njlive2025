16363c9e75cd21dcb03b93774ebb0c31fb7b4800
ChillibyteUK <dan@chillibyte.co.uk>
2025-08-18 09:01:12 +0100

    footer

--- footer.php
+++ footer.php
@@ -19,10 +19,10 @@ if ( ! is_page( 'contact' ) ) {
 <footer class="footer pb-4">
     <div class="container py-4">
         <div class="row g-3">
-            <div class="col-lg-3 text-center text-lg-start">
-				&copy; <?= esc_html( gmdate( 'Y' ) ); ?> NJ Live. All rights reserved.
+            <div class="col-12 text-center">
+				&copy; <?= esc_html( gmdate( 'Y' ) ); ?> NJ Live is part of the Human Network Group of companies, legally trading as NJ Live Ltd, a company registered in England and Wales Company Number 04786124.
             </div>
-            <div class="col-lg-6 text-center">
+            <div class="col-lg-6 offset-lg-3 text-center">
 				<a href="/privacy-policy/" class="text-decoration-none">Privacy Policy</a> |
 				<a href="/terms-and-conditions/" class="text-decoration-none">Terms & Conditions</a> |
 				<a href="/cookie-policy/" class="text-decoration-none">Cookie Policy</a> |
