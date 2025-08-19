54ec251317ae2ffebc1395f223e14fb36092e881
ChillibyteUK <dan@chillibyte.co.uk>
2025-08-19 13:44:02 +0100

    contact template title

--- page-templates/contact.php
+++ page-templates/contact.php
@@ -16,24 +16,27 @@ $socials = get_field( 'social', 'option' );
 		<div class="container pb-5">
 			<div class="row">
 				<div class="col-lg-6 py-lg-5 my-auto">
-					<h1>Contact us</h1>
+					<h1 data-aos="fade">Let's get started</h1>
 				</div>
 				<div class="col-lg-6 pb-4 py-lg-5 my-auto">
 					<ul class="fa-ul">
 						<?php
+						$d = 0;
 						if ( $socials['instagram_url'] ) {
 							?>
-							<li><span class="fa-li"><i class="fa-brands fa-instagram fa-2x"></i></span> <a href="<?= esc_url( $socials['instagram_url'] ); ?>"><?= $socials['instagram_url']; ?></a></li>
+							<li data-aos="fade-left" data-aos-delay="<?= esc_attr( $d ); ?>"><span class="fa-li"><i class="fa-brands fa-instagram fa-2x"></i></span> <a href="<?= esc_url( $socials['instagram_url'] ); ?>"><?= $socials['instagram_url']; ?></a></li>
 							<?php
+							$d += 50;
 						}
 						if ( $socials['linkedin_url'] ) {
 							?>
-							<li><span class="fa-li"><i class="fa-brands fa-linkedin fa-2x"></i></span> <a href="<?= esc_url( $socials['linkedin_url'] ); ?>"><?= $socials['linkedin_url']; ?></a></li>
+							<li data-aos="fade-left" data-aos-delay="<?= esc_attr( $d ); ?>"><span class="fa-li"><i class="fa-brands fa-linkedin fa-2x"></i></span> <a href="<?= esc_url( $socials['linkedin_url'] ); ?>"><?= $socials['linkedin_url']; ?></a></li>
 							<?php
+							$d += 50;
 						}
 						?>
-						<li><span class="fa-li"><i class="fa-solid fa-paper-plane fa-2x"></i></span> <a href="mailto:<?= antispambot( esc_html( get_field( 'contact_email', 'option' ) ) ); ?>"><?= esc_html( get_field( 'contact_email', 'option' ) ); ?></a></li>
-						<li><span class="fa-li"><i class="fa-solid fa-phone fa-2x"></i></span> <a href="tel:<?= esc_html( parse_phone( get_field( 'contact_phone', 'option' ) ) ); ?>"><?= esc_html( get_field( 'contact_phone', 'option' ) ); ?></a></li>
+						<li data-aos="fade-left" data-aos-delay="<?= esc_attr( $d + 100 ); ?>"><span class="fa-li"><i class="fa-solid fa-paper-plane fa-2x"></i></span> <a href="mailto:<?= antispambot( esc_html( get_field( 'contact_email', 'option' ) ) ); ?>"><?= esc_html( get_field( 'contact_email', 'option' ) ); ?></a></li>
+						<li data-aos="fade-left" data-aos-delay="<?= esc_attr( $d + 150 ); ?>"><span class="fa-li"><i class="fa-solid fa-phone fa-2x"></i></span> <a href="tel:<?= esc_html( parse_phone( get_field( 'contact_phone', 'option' ) ) ); ?>"><?= esc_html( get_field( 'contact_phone', 'option' ) ); ?></a></li>
 					</ul>
 				</div>
 			</div>
