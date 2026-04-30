4f2fcdc04b774c76e76754801a8459bd13a3f43a
ChillibyteUK <dan@chillibyte.co.uk>
2026-04-30 09:48:25 +0100

    blog nav

--- single.php
+++ single.php
@@ -18,6 +18,7 @@ get_header();
 		</div> -->
 		<div class="row">
 			<div class="col-lg-12 my-auto">
+				<a href="/insights/" class="single-blog__back-link mb-3 d-block"><?= esc_html__( 'Back to Insights', 'cb-njlive2025' ); ?></a>
 				<h1><?= esc_html( get_the_title() ); ?></h1>
 				<div class="post_hero mb-4">
 					<?= get_the_post_thumbnail( get_the_ID(), 'full', array( 'class' => 'single-blog__image' ) ); ?>
